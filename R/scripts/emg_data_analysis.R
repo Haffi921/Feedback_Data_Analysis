d6e21ad7ab5d6928e5b00fd48514f3f69d446fe1
Hafsteinn Ragnarsson <haffi921@gmail.com>
2021-12-01 23:38:43 +0100

    Update emg_data_analysis.R
    Simplification

--- R/scripts/emg_data_analysis.R
+++ R/scripts/emg_data_analysis.R
@@ -33,18 +33,9 @@ df <- read_csv(
   mutate(Bin = as.numeric(Bin)) %>%
   
   # Create difference score
-  group_by(participant, Segment) %>%
+  group_by(participant, block, trial) %>%
   mutate(Corr_Diff = Corr - lag(Corr), Zygo_Diff = Zygo - lag(Zygo)) %>%
   ungroup() %>%
-  
-  # Create columns for Z calcuations
-  mutate(
-    Corr_Trial_Z = NA, Corr_Diff_Trial_Z =  NA, Zygo_Trial_Z = NA, Zygo_Diff_Trial_Z =  NA,
-    Corr_Bin_Z = NA, Corr_Diff_Bin_Z =  NA, Zygo_Bin_Z = NA, Zygo_Diff_Bin_Z =  NA, 
-  ) %>%
-  
-  # Create outlier column
-  mutate(Corr_Outlier = FALSE, Zygo_Outlier = FALSE) %>%
   
   # Remove unwanted columns
   select(-c(trial_block, made, correct))
@@ -53,24 +44,20 @@ glimpse(df)
 
 
 # Function for Z-Calculation
-ZCalc <- function(column, zcol) {
-  avg = mean(column, na.rm = TRUE)
-  std = sd(column, na.rm = TRUE)
-  zcol <- abs((column - avg) / std)
-  return(zcol)
+ZCalc <- function(column) {
+  avg <- mean(column, na.rm = TRUE)
+  std <- sd(column, na.rm = TRUE)
+  return(abs((column - avg) / std))
 }
 
 # Function for outlier detection
-outlier <- function(column, outliers) {
-  avg = mean(column, na.rm = TRUE)
-  std = sd(column, na.rm = TRUE)
-  
-  for(i in 1:length(column)) {
-    if(!is.na(column[i]) & abs((column[i] - avg) / std) > 3.5) {
-      outliers[i] = TRUE
-    }
+outlier <- function(..., initial = NULL, z = 3.5) {
+  columns <- list(...)
+  outliers <- if_else(is.null(initial), rep(FALSE, length(columns[1])), initial)
+  for(column in columns) {
+    outliers <- outliers | (ZCalc(column) > z)
   }
-  return(outliers)
+  return(replace(outliers, is.na(outliers), FALSE))
 }
 
 # Run outlier detection
@@ -79,53 +66,22 @@ df <- df %>%
   # Detect outliers within each individual trial
   group_by(participant, block, trial) %>%
   mutate(
-    Corr_Trial_Z = ZCalc(Corr, Corr_Trial_Z),
-    Corr_Diff_Trial_Z = ZCalc(Corr_Diff, Corr_Diff_Trial_Z),
-    Zygo_Trial_Z = ZCalc(Zygo, Zygo_Trial_Z),
-    Zygo_Diff_Trial_Z = ZCalc(Zygo_Diff, Zygo_Diff_Trial_Z),
+    Corr_Outlier = outlier(Corr, Corr_Diff),
+    Zygo_Outlier = outlier(Zygo, Zygo_Diff),
   ) %>%
   ungroup() %>%
   
   # Detect outliers within each individual bin, across trials
   group_by(participant, Bin) %>%
   mutate(
-    Corr_Bin_Z = ZCalc(Corr, Corr_Bin_Z),
-    Corr_Diff_Bin_Z = ZCalc(Corr_Diff, Corr_Diff_Bin_Z),
-    Zygo_Bin_Z = ZCalc(Zygo, Zygo_Bin_Z),
-    Zygo_Diff_Bin_Z = ZCalc(Zygo_Diff, Zygo_Diff_Bin_Z),
-  ) %>%
-  ungroup() %>%
-  
-  # Detect outliers within each individual trial
-  group_by(participant, block, trial) %>%
-  mutate(
-    Corr_Outlier = outlier(Corr, Corr_Outlier),
-    Zygo_Outlier = outlier(Zygo, Zygo_Outlier),
-    Corr_Outlier = outlier(Corr_Diff, Corr_Outlier),
-    Zygo_Outlier = outlier(Zygo_Diff, Zygo_Outlier),
-  ) %>%
-  ungroup() %>%
-  
-  # Detect outliers within each individual bin, across trials
-  group_by(participant, Bin) %>%
-  mutate(
-    Corr_Outlier = outlier(Corr, Corr_Outlier),
-    Zygo_Outlier = outlier(Zygo, Zygo_Outlier),
-    Corr_Outlier = outlier(Corr_Diff, Corr_Outlier),
-    Zygo_Outlier = outlier(Zygo_Diff, Zygo_Outlier),
+    Corr_Outlier = outlier(Corr, Corr_Diff),
+    Zygo_Outlier = outlier(Zygo, Zygo_Diff),
   ) %>%
   ungroup()
 
 # Collect outliers for inspection
 outliers <- df %>%
   filter(Corr_Outlier | Zygo_Outlier)
-
-# Remove Z calc columns
-df <- df %>%
-  select(-c(
-    Corr_Trial_Z, Corr_Diff_Trial_Z, Zygo_Trial_Z, Zygo_Diff_Trial_Z,
-    Corr_Bin_Z, Corr_Diff_Bin_Z, Zygo_Bin_Z, Zygo_Diff_Bin_Z, 
-  ))
 
 # Create specific dataframes for each
 Corr_df <- df %>%
@@ -136,7 +92,7 @@ Zygo_df <- df %>%
   filter(!Zygo_Outlier) %>%
   select(-c(Corr, ends_with("Outlier")))
 
-l <- aggregate(Corr ~ congruencyNmin1 + congruency + feedback, df, mean)
+l <- aggregate(Corr ~ Bin + congruencyNmin1 + congruency + feedback, df, mean)
 l
 
 model <- aov(Corr ~ feedback * congruency * congruencyNmin1 * Bin, Corr_df)
